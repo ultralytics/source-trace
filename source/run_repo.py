f03144281ab07f5cc6618472980c59b48a7e5f45
UltralyticsAssistant <web@ultralytics.com>
2024-04-07 16:37:48 +0000

    Auto-format by https://ultralytics.com/actions

--- source/run_repo.py
+++ source/run_repo.py
@@ -4,8 +4,8 @@ from git import Repo
 
 from source import ROOT
 
-SOURCE_REPO = 'https://github.com/ultralytics/ultralytics'  # source repo
-DEST_REPO = 'https://github.com/ultralytics/yolov5'
+SOURCE_REPO = "https://github.com/ultralytics/ultralytics"  # source repo
+DEST_REPO = "https://github.com/ultralytics/yolov5"
 
 SUFFIXES = (".py",)
 IGNORE_START = ("import", "from", "try", "except", "with", "@pytest")
@@ -58,15 +58,16 @@ def clone_repository(repo_url, clone_to):
 
 def extract_file_contents(repo_path):
     """Read all text-readable files in the local directory of a repository to extract their contents, ignoring binary
-    files."""
+    files.
+    """
     repo_path = Path(repo_path)
     contents = {}
-    binary_extensions = {'.png', '.jpg', '.jpeg', '.gif', '.ico', '.bin', '.exe', '.dll', '.so', '.zip'}
+    binary_extensions = {".png", ".jpg", ".jpeg", ".gif", ".ico", ".bin", ".exe", ".dll", ".so", ".zip"}
     # Traverse through all files in the repository directory
-    for file_path in repo_path.rglob('*'):
+    for file_path in repo_path.rglob("*"):
         if file_path.is_file() and file_path.suffix not in binary_extensions:
             try:
-                with file_path.open('r', encoding='utf-8') as file:
+                with file_path.open("r", encoding="utf-8") as file:
                     contents[str(file_path.relative_to(repo_path))] = [line.strip().lower() for line in file]
             except UnicodeDecodeError:
                 pass
@@ -94,9 +95,9 @@ def compare_repos(repo_a_contents, repo_b_contents):
                 if any(common_lines):
                     for line in common_lines:
                         if (
-                                len(line) > 20
-                                and not any(line.startswith(x) for x in IGNORE_START)
-                                and not any(line == x for x in IGNORE_LINES)
+                            len(line) > 20
+                            and not any(line.startswith(x) for x in IGNORE_START)
+                            and not any(line == x for x in IGNORE_LINES)
                         ):
                             print(line)
                             copied_lines.append((file_a, file_b, line))
@@ -125,8 +126,8 @@ def main(repo_a, repo_b, local_dir):
     return copied_lines, stats
 
 
-if __name__ == '__main__':
-    local_dir = ROOT / 'github.com'
+if __name__ == "__main__":
+    local_dir = ROOT / "github.com"
     copied_lines, stats = main(SOURCE_REPO, DEST_REPO, local_dir=local_dir)
     # print("Copied Lines:", copied_lines)
     # print("Statistics:", stats)
