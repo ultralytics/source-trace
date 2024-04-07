6ca748171dfe5b675d436434a297009e2877f41a
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-04-07 17:00:47 +0200

    Initial commit

---
+++ template/module1.py
@@ -0,0 +1,15 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+
+
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the sum.
+
+    Args:
+        a (float | int): The first number to add.
+        b (float | int): The second number to add.
+
+    Returns:
+        (float | int): The sum of `a` and `b`.
+    """
+    return a + b
